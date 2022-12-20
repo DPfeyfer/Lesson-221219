17cf2db59d397513997400d1b0b034373855074d
DPfeyfer <Skydenfer737@gmail.com>
2022-12-20 12:04:44 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,24 @@ public class Main {
         System.out.println(dog);
         dog = dog - 3.5;
         System.out.println(dog);
+
+
         var cat = 3.6;
         System.out.println(cat);
         cat = cat + 4;
         System.out.println(cat);
         cat = cat - 1.6;
         System.out.println(cat);
+
+
         var paper = 763789;
         System.out.println(paper);
         paper = paper + 4;
         System.out.println(paper);
         paper = paper - 7639;
         System.out.println(paper);
+
+
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
@@ -25,5 +31,20 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
 
-        }
-}
+
+        var frog = 3.5;
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
+        var boxerPiter = 78.2;
+        var boxerIvan = 82.7;
+        var fullWeight = boxerPiter + boxerIvan;
+        System.out.println("Full weight boxer is " + fullWeight + " kg!");
+        var differenceWeight = boxerIvan - boxerPiter;
+        System.out.println("Difference weight between boxer is " + differenceWeight + " kg!");}}
+
